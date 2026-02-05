b53159ff65e3c5d774854bbf200687e4cd2b27d9
Alexandru Scripchin <sripchin.sasa@gmail.com>
2026-02-05 21:10:53 +0200

    Adaug clasa Person

---
+++ exemplul1.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, age):
+        self._age = age
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            print("Varsta invalida")
+            return
+        self._age = value
